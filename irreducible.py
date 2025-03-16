ab03dba8cfea1136b37a1596fe9aeba968eb3d65
incommunicado <adriandu31@gmail.com>
2025-03-16 15:31:30 +0100

    Update irreducible.py

--- irreducible.py
+++ irreducible.py
@@ -3,16 +3,14 @@ from polynomial_class import *
 q = { 1: [[1]] }
 
 def decompose(n):
-    #executes quickly approx up to 60
+    #returns integer partitions of n, eg. 4 -> [[4],[3,1],[2,2],[2,1,1],[1,1,1,1]]
+    #executes quickly approx up to n = 60
     try:
         return q[n]
-        #for part in q[n]:
-        #    yield part
     except:
         pass
 
     result = [[n]]
-    #yield [n]
 
     for i in range(1, n):
         a = n-i
@@ -20,27 +18,9 @@ def decompose(n):
         for r in R:
             if r[0] <= a:
                 result.append([a] + r)
-                #yield ([a] + r)
 
     q[n] = result
-    #result.remove([n])
     return result
-
-'''
-def decompose_iter(n):
-    #returns an iterable
-    # n = a + b
-    if n == 1:
-        yield [1]
-
-    for i in range(1,n):
-        a = n-i
-        for b in decompose_iter(i):
-            if b[0] <= a:
-                yield [a] + b
-
-    #MUCH SLOWER THAN ITS NON-ITERABLE COUNTERPART
-'''
 
 def exhaust(part):
     #takes a partition of an integer as input, and returns a list of every product of irreducible polys with degree corresponding to the partition
@@ -60,7 +40,6 @@ def exhaust(part):
         for P in Irred[part[0]]:
             res += [P*Q for Q in products_before]
 
-        #print(res)
         return res
     
 def PolyGen(n,mod):
@@ -68,8 +47,6 @@ def PolyGen(n,mod):
     #sorted by degree (in a dictionary -> res)
     deg = 2
     res = {2:[Polynomial({2:GaloisFp(1,mod)})]}
-    #print(res)
-    #b = str(input('continue ? '))
     while deg <= n:
         curr = res[deg][-1].copy()
         not_done = True
@@ -85,11 +62,7 @@ def PolyGen(n,mod):
                 deg += 1
                 res[deg] = []
                 curr = {deg:GaloisFp(1,mod)}
-                #print(curr)
-                #b = str(input('continue ? '))
         res[deg].append(Polynomial(curr))
-        #print(res)
-        #b = str(input('continue ? '))
 
     res[deg].pop(-1) #pops the polynomial X^{n+1} that eventually gets added
 
@@ -105,49 +78,26 @@ Polys = PolyGen(n,p)
 start1 = time.time()
 
 for deg in range(2,n+1):
+    #sieve method
+    #at the end, shows irreducible polynomials of degree up to n, in Z/pZ
     Reducible = []
     for part in decompose(deg):
         if part != [deg]:
             try:
                 Reducible += exhaust(part)
-            except:
-                print(Irred)
-                print('irred')
-                print(part)
-                print('partition')
-                print(str(input('continue ? ')))
-        '''
-        for prod in exhaust(part):
-            try:
-                Reducible = Reducible + prod
-            except:
-                print(type(Reducible))
-                print(type(prod))
-                print(str(input('continue ? ')))
-        '''
 
     for P in Reducible:
         for Q in Polys[deg]:
             if (not (P-Q)):
                 Polys[deg].remove(Q)
 
-    #print(Reducible)
-    #print('these are reducible polys')
-
     Irred[deg] = [P for P in Polys[deg]]
-
-    #print(Irred[deg])
-    #print(str(input('continue ? ')))
 
 end1 = time.time()
 
 querry = str(input('continue ? '))
 print({key:len(Irred[key]) for key in Irred.keys()})
 print(str(end1 - start1) + ' seconds taken')
-
-#print(len(PolyGen(7,p)))
-
-#TODO write another algorithm which takes every product and instead of doing every unique mult, multiply each irred by previously obtained polynomials (may be composite)
 
 Irred = {1:[Polynomial([GaloisFp(x,p),GaloisFp(1,p)]) for x in range(p)]}
 
@@ -160,7 +110,7 @@ start2 = time.time()
 Reducible = []
 
 for deg in range(2,n+1):
-
+    #other method where instead of sieving according to every unique product we do every multiplication (might encounter multiples, for example (X-1)*(X^2 + X) and (X^2 - 1)*X alike)
     for k in Irred.keys():
         for P in Irred[k]:
             for Q in Polys[deg-k]:
@@ -178,3 +128,5 @@ end2 = time.time()
 querry = str(input('continue ? '))
 print({key:len(Irred[key]) for key in Irred.keys()})
 print(str(end2 - start2) + ' seconds taken')
+
+#at the end both methods return the same irreducible polynomials, and the time taken with each method is compared
